9cb3a22ff054c7d00d482963623b95dc3a9af379
pgiotis <giotis.p@gmail.com>
2013-06-17 13:38:28 +0000

    -Add stereotype class for the rest api. -modify pbeans.in with the rest stereotype mode

---
+++ PServer/PServer/src/pserver/restPServlets/SgetUsersStereotypes.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pserver.restPServlets;
+
+import pserver.data.DBAccess;
+import pserver.data.VectorMap;
+
+/**
+ *
+ * @author Panagiotis Giotis <giotis.p@gmail.com>
+ */
+public class SgetUsersStereotypes  implements pserver.pservlets.PService{
+
+    private String responseType = pserver.pservlets.PService.xml;
+
+    /**
+     * Returns the mime type.
+     *
+     * @return Returns the XML mime type from Interface {@link PService}.
+     */
+    @Override
+    public String getMimeType() {
+        return responseType;
+    }
+
+    @Override
+    public void init(String[] params) throws Exception {
+        if (params.length < 1) {
+            return;
+        }
+        if (params[0].endsWith("xml")) {
+            responseType = pserver.pservlets.PService.xml;
+        } else {
+            responseType = pserver.pservlets.PService.json;
+        }
+    }
+
+    @Override
+    public int service(VectorMap parameters, StringBuffer response, DBAccess dbAccess) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
